3676a945fcf2908f1c87d3271689d1364b27ed07
Jonathan Massoué <j.massoue@hotmail.fr>
2020-07-01 12:35:20 +0200

    page d'accueil

--- view/frontend/home.php
+++ view/frontend/home.php
@@ -5,19 +5,58 @@
 <html lang="fr">
 
 	<head>
-		<?php include("view/include/head.php"); ?>
+		<?php include("../include/head.php"); ?>
 	</head>
 
 	<body>
-		<img src="public/images/" alt="photomontagnealaska" class="alaska col-lg-12">
+		<!--<img src="public/images/" alt="sucredansduthé" class="sugar_tea" col-lg-12"> -->
         <h1> Le Temple du Pharaon</h1>
             <p>Salon de thé, chicha & plats à Brest</p>
 		<br>
-		
-		
+		<div class='présentation'>
+			<h2>Le Temple Du Pharaon</h2>
+			<h3>Un lieu atypik à Brest</h3>
+
+			<p>Situé depuis 15 ans au plein cœur du centre ville de Brest, Le Temple Du Pharaon est un lieu unique sur le Nord Finistère. Son propriétaire, Mamdouh, vous reçoit dans une ambiance familiale et décontractée inspirée de son pays d’origine. La décoration traditionnelle et l’aménagement intérieur vous transporte instantanément en Egypte.</p>
+			
+			<p>Un grand choix de narguilés, une carte de thés aux saveurs d’Orient, des mezzés … L’ensemble de la carte est concoctée par Mamdouh à partir de produits bios soigneusement sélectionnés par ses soins.</p>
+			
+			<p>Le Temple du Pharaon organise régulièrement des soirées de danses orientales, musiques ainsi que récitation de contes. Il est aussi possible d’y organiser des soirées privées pour vos anniversaires, EVJF/EVG …</p>
+		</div>
+		<!--<img src="public/images/" alt="thésurunetable" class="tea_table" col-lg-12"> -->
+
+		<div class='emporter'>
+			<h2>Vos chichas et toute la carte</h2>
+			<h3>à emporter dès le 3 juin 2020 sur Brest</h3>
+
+			<div class="p1">
+				<p>En cette période de déconfinement, Le Temple Du Pharaon situé à Brest se ré-invente.  Vous pouvez à présent commander à emporter l’ensemble de notre carte. Cocktails, thés, boissons chaudes, pâtisseries, narguilés …  Tous nos ingrédients sont bios et préparés avec soins par Mamdouh. La boutique est aussi ré-ouverte.</p>
+			</div>
+			<div class='p2'>
+				<h3>27.00 euros</h3>
+				<h4>location chicha avec tabac</h4>
+					<p>Récupérez votre chicha de 17h00 à 23h00 pour un retour entre 15h00 et 17h00 le lendemain. Pour 27€ nous vous fournissons la chicha avec 3 charbons et le tabac.</p>
+			</div>
+			<div class='p3'>
+				<h3>20.00 euros</h3>
+				<h4>location chicha sans tabac</h4>
+					<p>Récupérez votre chicha de 17h00 à 23h00 pour un retour entre 15h00 et 17h00 le lendemain. Pour 20€ nous vous fournissons une chicha mais sans tabac ni charbon.</p>
+			</div>
+		</div>
+		<div>
+			<h2>Découvrez nos soirées</h2>
+			<h3>danses, musiques et contes</h3>
+
+			<video class='danse' src="https://www.letemple-dupharaon.fr/wp-content/uploads/2020/06/soiree-danse-oriental-le-temple-du-pharaon-Brest.mp4" controls controlslist="nodownload"></video>
+			<video class='music' src="https://www.letemple-dupharaon.fr/wp-content/uploads/2020/06/soiree-musique-le-temple-du-pharaon-brest.mp4" controls controlslist="nodownload"></video>
+			<video class='music' src="https://www.letemple-dupharaon.fr/wp-content/uploads/2020/06/soiree-contes-le-temple-du-pharaon-brest.mp4#t=,30" controls controlslist="nodownload"></video>
+		</div>
+		<div class="map">
+		<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1324.811081637734!2d-4.4888140000000005!3d48.386994!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4816b959ee18dee3%3A0x4eac4b1af016f1aa!2s36%20Rue%20Jean%20Mac%C3%A9%2C%2029200%20Brest%2C%20France!5e0!3m2!1sfr!2sus!4v1593599346588!5m2!1sfr!2sus" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+		</div>
 		
 		<footer>
-			<?php include("view/include/footer.php"); ?>
+			<?php include("../include/footer.php"); ?>
 		</footer>
 	</body>
 </html>
---
+++ view/index.php
@@ -0,0 +1,16 @@
+<?php 
+ session_start();
+ ?>
+<!DOCTYPE html>
+<html lang="fr">
+	<body>
+
+		<header>
+			<?php include("head.php"); ?>
+		</header>
+
+		<footer>
+			<?php include("footer.php"); ?>
+		</footer>
+	</body>
+</html>
